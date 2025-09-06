ee59104e8cfe60b374b65ccd9f5201632ee2a82b
Karen Sonnenberg <kjsonnenberg@me.com>
2025-09-06 16:39:25 -0700

    1st commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './templates/**/*.html',
+    './core/templates/**/*.html',
+    './core/**/*.py',
+    './website/**/*.py',
+    './static/src/**/*.js',
+    './static/src/css/**/*.css',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
